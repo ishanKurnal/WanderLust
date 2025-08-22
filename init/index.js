ecbf2cbc9da662e67bf0510144642782ee1810b2
Ishan Kurnal <ikurnal@gmail.com>
2025-08-22 20:08:13 +0530

    Final changes

--- init/index.js
+++ init/index.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose'); // Importing mongoose for MongoDB interaction
 const initData = require('./data'); // Importing initial data for the database
 const Listing = require('../models/listing'); // Importing the Listing model
+const path = require('path');
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config({ path: path.resolve(__dirname, '../.env') });
+}
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust'; // MongoDB connection URL
+const dbURL = process.env.ATLASDB_URL; // MongoDB connection URL
 
 main()
   .then((res) => {
@@ -11,12 +15,12 @@ main()
   .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_URL); // Connecting to MongoDB database named 'wanderlust'
+  await mongoose.connect(dbURL); // Connecting to MongoDB database named 'wanderlust'
 }
 
 const initDB = async () => {
     await Listing.deleteMany({}); // Clearing the existing listings in the database
-    initData.data = initData.data.map((obj) => ({ ...obj, owner: '689cdc93c877498590959bbe'}));
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: '68a84e0181aff1fd571eaee9'}));  
     await Listing.insertMany(initData.data); // Inserting initial data into the listings collection
     console.log("Database initialized with sample data!"); // Logging the initialization status
 };
